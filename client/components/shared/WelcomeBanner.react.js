f293c26d53387926acf60f9a6b6781d1b7ee8d0d
copecopecope <alexjcope@gmail.com>
2016-09-15 12:55:02 -0700

    Fix signup prompt bug

--- client/components/shared/WelcomeBanner.react.js
+++ client/components/shared/WelcomeBanner.react.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 class WelcomeBanner extends React.Component {
-    constructor(props) {
-        super(props);
-        this.displayName = 'WelcomeBanner';
-    }
+
     render() {
         return (
             <div className="app-banner welcome-banner">
                 <div className="app-banner_prompt" onClick={this.handleClick} >
-                    <div>Open in app</div>
+                    { this.props.children }
                 </div>
                 <div className="app-banner_close" onClick={this.handleClose} >
                     <Icon type="close" />
