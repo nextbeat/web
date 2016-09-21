e59510cd6416465372ea7294a092f6cca2636ac5
copecopecope <alexjcope@gmail.com>
2016-09-21 13:31:16 -0700

    Password authentication for dev site

--- docker/nginx/init.sh
+++ docker/nginx/init.sh
@@ -2,8 +2,14 @@
 set -e
 
 # Replace environment-specific variables in web.conf
-echo "$SERVER_NAME"
 sed -i "s/SERVER_NAME/$SERVER_NAME/g" /etc/nginx/nginx.conf
+
+# If in development environment, add password auth
+if [[ $REQUIRE_AUTH == "true" ]]; then
+    sed -i "/location \/ {/a \
+\            auth_basic \"Restricted Content\";\n\
+\            auth_basic_user_file /etc/nginx/.htpasswd;" /etc/nginx/nginx.conf
+fi
 
 nginx -g "daemon off;"
 
