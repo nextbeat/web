1536a8ead02f250e6b84549325d483eaf831874c
copecopecope <alexjcope@gmail.com>
2017-05-08 14:59:43 -0700

    React production build

--- config/webpack.dll.production.js
+++ config/webpack.dll.production.js
@@ -2,11 +2,16 @@ const webpack               = require('webpack');
 const merge                 = require('webpack-merge');
 const config                = require('./webpack.dll.base');
 
+const nodeEnv = process.env.NODE_ENV || 'development';
+
 module.exports = merge(config, {
     output: {
         filename: '[name].[chunkhash].dll.js'
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(nodeEnv)
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false,
