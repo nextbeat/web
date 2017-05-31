2ba7b38fff8d214ac196d35261e382344baf15bb
copecopecope <alexjcope@gmail.com>
2017-05-31 12:25:39 -0700

    Links and proper element creation

--- client/components/room/chat/utils/renderMessageText.js
+++ client/components/room/chat/utils/renderMessageText.js
@@ -1,98 +1,23 @@
 import React from 'react'
 import inRange from 'lodash/inRange'
+import assign from 'lodash/assign'
 import { List } from 'immutable'
 import { hashCode } from '../../../../utils'
 
-// function renderWithRegexMentions(message, { start, end, onMentionClick }) {
-//     const re = /(@\w+)/
-
-//     return message.substring(start, end).split(re).map((str, idx) => {
-//         const key = `@${idx}`
-//         if (str.charAt(0) === '@') {
-//             const username = str.substring(1)
-//             return <a key={key} className="chat_mention" onClick={onMentionClick.bind(this, username)}>{str}</a>
-//         }
-//         return <span key={key}>{str}</span>
-//     })
-// }
-
-// function renderWithMentions(comment, { start, end, onMentionClick, forceMentions=false }) {
-//     if (!comment.get('message')) {
-//         return null
-//     }
-    
-//     let message = comment.get('message')
-//     start = start || 0
-//     end = end || message.length
-//     onMentionClick = onMentionClick || (() => {})
-
-//     if (forceMentions) {
-//         // if specified, render as a mention any word starting with the @ symbol
-//         // and ignore the user_mentions data
-//         return renderWithRegexMentions(message, { start, end, onMentionClick })
-//     }
-
-//     let mentions = comment.get('user_mentions') || List()
-//     let inclusiveMentions = mentions.filter(m => inRange(m.get('indices').get(0), start, end))
-
-//     // construct an array of <span>s using index data in mentions
-//     let elems   = [] 
-//     let idx     = start // keeps track of current index of string
-//     inclusiveMentions.forEach(m => {
-//         const [mStart, mEnd] = m.get('indices').toJS()
-//         elems.push(<span key={`${idx},${mStart}`}>{ message.substring(idx, mStart) }</span>)
-
-//         const mentionKey = `${mStart},${mEnd}`
-//         const url = `/u/${m.get('username')}`
-//         elems.push(
-//             <a key={mentionKey} className="chat_mention" onClick={onMentionClick.bind(this, m.get('username'))}>
-//                 { message.substring(mStart, mEnd) }
-//             </a>
-//         )
-
-//         idx = mEnd
-//     })
-//     elems.push(<span key={`${idx},${end}`}>{ message.substring(idx, end) }</span>)
-
-//     return elems
-// }
-
-
-// export default function renderMessageText(comment, { onMentionClick, forceMentions=false, includeLinks=false } = {}) {
-//     if (!includeLinks) {
-//         return renderWithMentions(comment, { onMentionClick, forceMentions })
-//     }
-
-//     // if includeLinks is true, format URLs using Markdown format [display](url)
-//     var re = /\[(.+)\]\((.+)\)/g 
-
-//     let elems = []
-//     let key = 0
-//     let lastIndex = 0
-//     let result
-
-//     while (result = re.exec(comment.get('message'))) {
-//         elems.push(<span key={key}>{renderWithMentions(comment, { start: lastIndex, end: result.index, onMentionClick, forceMentions })}</span>)
-
-//         let displayText = result[1]
-//         let url = result[2]
-//         elems.push(<a target="_blank" rel="nofollow" href={url} key={key+1}>{displayText}</a>)
-
-//         key += 2
-//         lastIndex = re.lastIndex
-//     }
-
-//     elems.push(<span key={key}>{renderWithMentions(comment, { start: lastIndex, onMentionClick, forceMentions })}</span>)
-
-//     return elems
-
-// }
-
 function getLinkData(comment) {
-    return List(); // TODO
+    var re = /\[(.+)\]\((.+)\)/g 
+
+    let links = []
+    let result
+
+    while (result = re.exec(comment.get('message'))) {
+        links.push({ type: 'link', start: result.index, end: result.index+result[0].length, displayText: result[1], url: result[2] })
+    }
+
+    return links
 }
 
-function preprocessAnnotations(comment, { includeLinks=false } = {}) {
+function preprocessAnnotations(comment, { includeLinks=false }) {
     let mentions = comment.get('user_mentions') || List();
     let highlights = comment.get('result_indices') || List();
     let links = includeLinks ? getLinkData(comment) : List();
@@ -104,46 +29,78 @@ function preprocessAnnotations(comment, { includeLinks=false } = {}) {
     highlights.forEach(h => {
         annotations = annotations.push({ type: 'highlight', start: h.get(0), end: h.get(1) })
     })
-    
-    // todo: links
+    links.forEach(l => {
+        annotations = annotations.push(l)
+    })
     
     return annotations.sort((a1, a2) => a1.start - a2.start || a2.end - a1.end)
 }
 
-function recursiveCreateElement(start, end, annotations, message) {
+function elementForAnnotation(annotation, annotations, message, options) {
+    let type;
+    let props = { 
+        className: `chat_item_${annotation.type}`, 
+        key: `${annotation.type},${annotation.start},${annotation.end}`
+    };
+    let textStart = annotation.start;
+    let textEnd = annotation.end;
+
+    let onMentionClick = options.onMentionClick || (() => {})
+
+    if (annotation.type === 'mention') {
+        type = 'a';
+        assign(props, { onClick: onMentionClick.bind(this, annotation.username) });
+    } else if (annotation.type === 'highlight') {
+        type = 'span';
+    } else if (annotation.type === 'link') {
+        type = 'a';
+        assign(props, { 
+            target: "_blank",
+            rel: "nofollow",
+            href: annotation.url
+        })
+        textStart = textStart+1
+        textEnd = textStart+annotation.displayText.length
+    }
+
+    return React.createElement(
+        type, 
+        props, 
+        recursiveCreateElement(textStart, textEnd, annotations.filter(a => a.start >= textStart && a.end <= textEnd), message, options)
+    );
+}
+
+function recursiveCreateElement(start, end, annotations, message, options) {
 
     if (annotations.size === 0) {
         return message.substring(start, end);
     }
 
-    let annotation = annotations.get(0)
-    annotations = annotations.shift()
+    let annotation = annotations.get(0);
+    annotations = annotations.shift();
+
     return [
-        recursiveCreateElement(start, annotation.start, List(), message),
-        React.createElement(
-            'span',
-            {className: annotation.type, key: `${annotation.type},${annotation.start},${annotation.end}`},
-            recursiveCreateElement(annotation.start, annotation.end, annotations.filter(a => a.start >= annotation.start && a.end <= annotation.end), message)
-        ),
-        recursiveCreateElement(annotation.end, end, annotations.filter(a => a.start >= annotation.end), message)
+        recursiveCreateElement(start, annotation.start, List(), message, options),
+        elementForAnnotation(annotation, annotations, message, options),
+        recursiveCreateElement(annotation.end, end, annotations.filter(a => a.start >= annotation.end), message, options)
     ]
 }  
 
-function doRenderMessageText(comment) {
-    let annotations = preprocessAnnotations(comment)
+function doRenderMessageText(comment, options) {
+    let annotations = preprocessAnnotations(comment, options)
     let message     = comment.get('message')
-    return <span>{recursiveCreateElement(0, message.length, annotations, message)}</span>
+    return <span>{recursiveCreateElement(0, message.length, annotations, message, options)}</span>
 }
 
 let cache = {}
-function memoizedRenderMessageText(comment) {
-    let key = hashCode(comment.get('message') + JSON.stringify(comment.get('result_indices')))
+function memoizedRenderMessageText(comment, options) {
+    let key = hashCode(comment.get('message') + JSON.stringify(comment.get('result_indices')) + JSON.stringify(options))
     if (!cache[key]) {
-        cache[key] = doRenderMessageText(comment);
+        cache[key] = doRenderMessageText(comment, options);
     }
     return cache[key]
 }
 
-export default function renderMessageText(comment) {
-    return memoizedRenderMessageText(comment)
+export default function renderMessageText(comment, options={}) {
+    return memoizedRenderMessageText(comment, options)
 }
