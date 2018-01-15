80a216e873d25b151e326dea9db95c3e5ab99309
copecopecope <alexjcope@gmail.com>
2018-01-15 11:48:03 -0800

    Disabling inline playback

--- client/components/room/player/Video.tsx
+++ client/components/room/player/Video.tsx
@@ -674,7 +674,7 @@ class Video extends React.Component<Props, VideoState> {
 
         let videoAttributes: any = {
             preload: "none",
-            playsInline: true,
+            // playsInline: true,
             controls: isIOS,
             poster: posterUrl
         }
