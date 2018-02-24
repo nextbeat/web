0fc96bd9f3051ea29e5882611b78b3d294f27851
copecopecope <alexjcope@gmail.com>
2018-02-23 16:41:15 -0800

    Advertising page redesign

--- client/components/pages/company/Advertising.tsx
+++ client/components/pages/company/Advertising.tsx
@@ -15,61 +15,72 @@ class Advertising extends React.Component {
                     <div className="advertising_intro_shape1" />
                     <div className="advertising_intro_shape2" />
                 </div>
-                <div className="advertising_users">
-                    <div className="advertising_content advertising_users_content">
-                        <div className="advertising_users_stats">
-                            <div className="advertising_users_stats_title">MINUTES SPENT IN ROOM PER USER</div>
-                            <div className="advertising_users_stats_body">
-                                <div className="advertising_users_stat">
-                                    <div className="advertising_users_stat_time">8:09</div>
-                                    <div className="advertising_users_stat_label">AVERAGE</div>
-                                </div>
-                                <div className="advertising_users_stat">
-                                    <div className="advertising_users_stat_time">52:55</div>
-                                    <div className="advertising_users_stat_label">TOP 10%</div>
-                                </div>
+                <div className="advertising_nextbeat">
+                    <div className="advertising_content advertising_nextbeat_content">
+                        <div className="advertising_text advertising_nextbeat_text">
+                            <div className="advertising_nextbeat_section">
+                                <h1>How does Nextbeat work?</h1>
+                                <p>Nextbeat is a video platform created by Youtubers for Youtubers, built to attract highly influential creators via a first-hand understanding of the problems they face, and run by influencers with social access to an elite tier of creators.</p>
+                                <p>On Nextbeat, creators can open a room and upload a series of short, punchy videos over the course of a few hours or a day, directly engaging their audience and fulfilling viewer requests with short video replies.</p>
+                            </div>
+                            <div className="advertising_nextbeat_section">
+                                <h1>Users hang out.</h1>
+                                <p>Because of Nextbeat's unique live-update format, audiences are left with time to kill between video drops. This downtime can be used to drive active buyers to your brand.</p>
                             </div>
                         </div>
-                        <div className="advertising_text advertising_users_text">
-                            <h1>Users hang out.</h1>
-                            <p>Because of Nextbeat's unique live-update format, audiences are left with time to kill between video drops. This downtime can be used to drive active buyers to your brand.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="advertising_nextbeat">
-                    <div className="advertising_nextbeat_container">
-                        <div className="advertising_nextbeat_info advertising_text">
-                            <h1>How does Nextbeat work?</h1>
-                            <p>Nextbeat is a video platform created by Youtubers for Youtubers, built to attract highly influential creators via a first-hand understanding of the problems they face, and run by influencers with social access to an elite tier of creators.</p>
-                            <p>On Nextbeat, creators can open a room and upload a series of short, punchy videos over the course of a few hours or a day, directly engaging their audience and fulfilling viewer requests with short video replies.</p>
-                        </div>
-                        <div className="advertising_nextbeat_stats">
-                            <div className="advertising_nextbeat_stat">Audiences watch an average of <b>82%</b> of each video</div>
-                            <div className="advertising_nextbeat_stat">Average number of videos in a room: <b>20</b></div>
+                        <div className="advertising_stats_column">
+                            <div className="advertising_stats">
+                                <div className="advertising_stat_group">
+                                    <div className="advertising_stat_group_title">AVERAGE PERCENT OF EACH VIDEO WATCHED</div>
+                                    <div className="advertising_stat_group_body">
+                                        <div className="advertising_stat">
+                                            <div className="advertising_stat_value">82%</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="advertising_stat_group">
+                                    <div className="advertising_stat_group_title">AVERAGE NUMBER OF VIDEOS PER ROOM</div>
+                                    <div className="advertising_stat_group_body">
+                                        <div className="advertising_stat">
+                                            <div className="advertising_stat_value">20</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="advertising_stat_group">
+                                    <div className="advertising_stat_group_title">MINUTES SPENT IN ROOM PER USER</div>
+                                    <div className="advertising_stat_group_body advertising_stat_group_body-multiple">
+                                        <div className="advertising_stat">
+                                            <div className="advertising_stat_value">8:09</div>
+                                            <div className="advertising_stat_label">AVERAGE</div>
+                                        </div>
+                                        <div className="advertising_stat">
+                                            <div className="advertising_stat_value">52:55</div>
+                                            <div className="advertising_stat_label">TOP 10%</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="advertising_store">
-                    <div className="advertising_store_triangle" />
                     <div className="advertising_store_header advertising_text">
                         <h1>Why partner with Nextbeat?</h1>
                     </div>
                     <div className="advertising_content advertising_store_content">
-                        <div className="advertising_store_icon">
-                            <div className="advertising_store_icon_inner advertising_store_icon-partnerships" />
+                        <div className="advertising_store_item">
+                            <div className="advertising_store_icon advertising_store_icon-partnerships" />
+                            <div className="advertising_text advertising_store_text">
+                                <h2>Strategic Partnerships</h2>
+                                <p>Align your brand with influential creators who speak directly into the ears of their millions of avid followers. Gain access to a young, enthusiastic demographic who view these content creators as equal parts idol and virtual best friend, and who take their recommendations very seriously.</p>
+                            </div>
                         </div>
-                        <div className="advertising_text advertising_store_text">
-                            <h2>Strategic Partnerships</h2>
-                            <p>Align your brand with influential creators who speak directly into the ears of their millions of avid followers. Gain access to a young, enthusiastic demographic who view these content creators as equal parts idol and virtual best friend, and who take their recommendations very seriously.</p>
-                        </div>
-                    </div>
-                    <div className="advertising_content advertising_store_content">
-                        <div className="advertising_store_icon">
-                            <div className="advertising_store_icon_inner advertising_store_icon-conversions" />
-                        </div>
-                        <div className="advertising_text advertising_store_text">
-                            <h2>Seamless Conversions</h2>
-                            <p>Unlike Youtube sponsorships, Nextbeat's integrated Shop tab makes it easy for consumers to convert tentative interest into tangible clickthroughs and purchases. Products featured in our Shop tab are easily accessible, and Nextbeat's low-maintenance format makes it possible for users to explore sponsored content without fear of getting left behind.</p>
+                        <div className="advertising_store_item">
+                            <div className="advertising_store_icon advertising_store_icon-conversions" />
+                            <div className="advertising_text advertising_store_text">
+                                <h2>Seamless Conversions</h2>
+                                <p>Unlike Youtube sponsorships, Nextbeat's integrated Shop tab makes it easy for consumers to convert tentative interest into tangible clickthroughs and purchases. Products featured in our Shop tab are easily accessible, and Nextbeat's low-maintenance format makes it possible for users to explore sponsored content without fear of getting left behind.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
